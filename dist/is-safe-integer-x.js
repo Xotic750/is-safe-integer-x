34ad29378120c6f705a5b3a404dac4b9969714e6
Graham Fairweather <xotic750@gmail.com>
2019-07-26 15:09:24 +0200

    :bookmark: v2.0.21
    Auto build

--- dist/is-safe-integer-x.js
+++ dist/is-safe-integer-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-25T21:49:07.904Z",
+  "date": "2019-07-26T13:09:17.744Z",
   "describe": "",
   "description": "Determine whether the passed value is a safe integer.",
   "file": "is-safe-integer-x.js",
   "hash": "302a1f18020b0acc1e30",
   "license": "MIT",
-  "version": "2.0.20"
+  "version": "2.0.21"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
